7333e039f1ea7f2f947202f257c1e870fb421617
luc457x <lucas7eixeir4@outlook.com>
2025-10-31 17:51:30 -0300

    done

--- scripts/content_handler.js
+++ scripts/content_handler.js
@@ -8,7 +8,7 @@ function carregarConteudo(pagina) {
             // Conteúdo dinâmico baseado no botão clicado
             const conteudos = {
                 'home': `
-                    <h1>Projeto Educação Ambiental nas Escolas</h1><br>
+                    <h1>Projeto Plantando Consciência</h1><br>
                     <h2>🌱 A importância da educação ambiental nas escolas</h2>
                     <p>A educação ambiental nas escolas é essencial para formar cidadãos conscientes, saudáveis e comprometidos com o futuro do planeta. 
                     Ao integrar práticas sustentáveis e o contato direto com a natureza desde a primeira infância, promovemos não apenas o aprendizado, 
@@ -26,22 +26,35 @@ function carregarConteudo(pagina) {
                      É preparar nossos alunos para serem protagonistas de um mundo mais justo e equilibrado.</p>
                 `,
                 'material': `
+                    <div class="material">
                     <h1>Material</h1><br>
-                    <p>A seguir estão reunidos os materiais de apoio para a realização do projeto. Os arquivos estão disponíveis para download em formato PDF, 
+                    <p>A seguir estão reunidos os materiais desenvolvidos para a realização do projeto.</p>
+                    <p>Os arquivos estão disponíveis para download em formato ZIP, contendo os arquivos em PDF e PNG, 
                     podendo ser impressos ou utilizados em dispositivos digitais para maior praticidade.</p><br>
-                    <a href="./docs/cultivar_e_transformar_para_criancas.pdf" download>Clique aqui para baixar o PDF</a>
-                    <a href="./docs/cultivar_e_transformar_para_jovens.pdf" download>Clique aqui para baixar o PDF</a>
-                    <a href="./docs/cultivar_e_transformar_para_jovens.pdf" download>Clique aqui para baixar o PDF</a>
-                    <a href="./docs/cultivar_e_transformar_para_jovens.pdf" download>Clique aqui para baixar o PDF</a>
-                    <a href="./docs/cultivar_e_transformar_para_jovens.pdf" download>Clique aqui para baixar o PDF</a><br>
-                    <h2>Como usar o material?</h2><br>
+                    <a href="./docs/projeto_plantando_consciencia.zip" download style="font-weight: bold; font-size: 1.2em;">Clique aqui para baixar o arquivo.</a><br>
+                    <br>
+                    <h2>Como usar o material?</h2>
                     <p>O material foi desenvolvido para ser utilizado pelo professor como um guia, podendo ser adaptado conforme a necessidade da turma e a realidade da escola.</p>
-                    <p>O material está dividido em duas partes, sendo a primeira um projeto de reciclagem e a segunda um projeto de horta comunitária.</p>
-                    <p>Cada projeto está subdivido em duas partes, uma para crianças dos anos iniciais e outra para jovens dos anos finais do ensino básico.</p>
-                    <p>Além do máterial, estão contidos manuais da Embrapa para auxiliar no desenvolvimento do projeto de horta comunitária.</p>
+                    <p>Ele é composto por duas partes, sendo a primeira um projeto de reciclagem, e a segunda um projeto de horta comunitária.
+                    Cada projeto está subdivido em duas partes para melhor adaptação a idade dos alunos cursando o ensino fundamental, uma para crianças dos anos iniciais, 
+                    e outra para jovens dos anos finais.</p>
+                    <p>Além do máterial desenvolvido, estão contidos dois manuais da Embrapa para auxiliar no desenvolvimento do projeto de horta comunitária.</p>
+                    </div>
                 `,
                 'contato': `
-                    <h2>Contato</h2>
+                <div class="contatos">
+                    <h1>Contato</h1>
+                    <p>Para mais informações sobre o projeto, entre em contato comigo, será um prazer ajudar!</p>
+                      <div class="contatos-item">
+                        <img src="../img/wpp.png" alt="whatsapp">
+                        <a href="https://api.whatsapp.com/send?phone=5522998923719" target="_blank">+55 (22) 99892-3719</a>
+                      </div>
+                      <div class="contatos-item">
+                        <img src="../img/e-mail.webp" alt="e-mail">
+                        <a href="mailto:contato@lucas7x.win" target="_blank">contato@lucas7x.win</a>
+                      </div>
+                      <img src="../img//qr.gif" alt="QR Code">
+                  </div>
                 `
             };
             
